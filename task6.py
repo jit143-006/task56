cc56ef6955502cc4f2d0b5bce57fafb00e71ac0c
jit143-006 <centralministryofeducation@gmail.com>
2025-08-16 13:16:09 +0530

    Create task6.py

---
+++ task6.py
@@ -0,0 +1,9 @@
+
+import math
+num=float(input("enter a number: "))
+square= math.sqrt(num)
+log= math.log(num)
+sine= math.sin(num)
+print(f"square root: {square}")
+print(f"logarithm: {log}")
+print(f"sine: {sine}")
